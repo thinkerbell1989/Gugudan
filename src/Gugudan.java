4d52df8451e7b0bdccaea9cfdf6298ff6ddbf294
Seongrok <Seongrok@Seongrok-PC>
2017-06-30 23:05:04 +0900

    6,7단 while & for

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
+	
+		// 6단
+		int i = 1;
+		while(i < 10) {
+			System.out.println(6 * i);
+			i = i + 1; // i++
+		}
+		
+		// 7단
+		for(int j = 1; j < 10; j++) {
+			System.out.println(7 *j);
+		}
+		
 		// 2단
 //		System.out.println("2 단");
 //		System.out.println(2*1);
@@ -26,7 +39,7 @@ public class Gugudan {
 //		System.out.println(3*8);
 //		System.out.println(3*9);
 				
-		// 4e단
+		// 4 단
 //		System.out.println("4단");
 //		int result=4*1;
 //		System.out.println(result);
@@ -47,18 +60,20 @@ public class Gugudan {
 //		result=4*9;
 //		System.out.println(result);
 		
-		System.out.println("구구단 중 출력할 단은?:");
-		Scanner scanner=new Scanner(System.in);
-		int number=scanner.nextInt();
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
+//		System.out.println("구구단 중 출력할 단은?:");
+//		Scanner scanner=new Scanner(System.in);
+//		int number=scanner.nextInt();
+//		System.out.println(number * 1);
+//		System.out.println(number * 2);
+//		System.out.println(number * 3);
+//		System.out.println(number * 4);
+//		System.out.println(number * 5);
+//		System.out.println(number * 6);
+//		System.out.println(number * 7);
+//		System.out.println(number * 8);
+//		System.out.println(number * 9);
+		
+		
 		
 	}
 
