701c8b2f360f5baa7563a407ff8d8a7ed1bd35ad
Seongrok <Seongrok@Seongrok-PC>
2017-07-05 00:03:55 +0900

    구구단 클래스 활용

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,80 +1,19 @@
-import java.util.Scanner;
 
 public class Gugudan {
-	public static void main(String[] args) {
-	
-		// 6단
-		int i = 1;
-		while(i < 10) {
-			System.out.println(6 * i);
-			i = i + 1; // i++
-		}
-		
-		// 7단
-		for(int j = 1; j < 10; j++) {
-			System.out.println(7 *j);
-		}
-		
-		// 2단
-//		System.out.println("2 단");
-//		System.out.println(2*1);
-//		System.out.println(2*2);
-//		System.out.println(2*3);
-//		System.out.println(2*4);
-//		System.out.println(2*5);
-//		System.out.println(2*6);
-//		System.out.println(2*7);
-//		System.out.println(2*8);
-//		System.out.println(2*9);
-		
-		// 3단
-//		System.out.println("3 단");
-//		System.out.println(3*1);
-//		System.out.println(3*2);
-//		System.out.println(3*3);
-//		System.out.println(3*4);
-//		System.out.println(3*5);
-//		System.out.println(3*6);
-//		System.out.println(3*7);
-//		System.out.println(3*8);
-//		System.out.println(3*9);
-				
-		// 4 단
-//		System.out.println("4단");
-//		int result=4*1;
-//		System.out.println(result);
-//		result=4*2;
-//		System.out.println(result);
-//		result=4*3;
-//		System.out.println(result);
-//		result=4*4;
-//		System.out.println(result);
-//		result=4*5;
-//		System.out.println(result);
-//		result=4*6;
-//		System.out.println(result);
-//		result=4*7;
-//		System.out.println(result);
-//		result=4*8;
-//		System.out.println(result);
-//		result=4*9;
-//		System.out.println(result);
-		
-//		System.out.println("구구단 중 출력할 단은?:");
-//		Scanner scanner=new Scanner(System.in);
-//		int number=scanner.nextInt();
-//		System.out.println(number * 1);
-//		System.out.println(number * 2);
-//		System.out.println(number * 3);
-//		System.out.println(number * 4);
-//		System.out.println(number * 5);
-//		System.out.println(number * 6);
-//		System.out.println(number * 7);
-//		System.out.println(number * 8);
-//		System.out.println(number * 9);
-		
-		
-		
-	}
 
-}
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for(int i = 0; i < result.length; i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+
+	}
